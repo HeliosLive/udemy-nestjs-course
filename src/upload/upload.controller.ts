32e7fc8824d8347dc77379555b857e949ac4a0bb
Ahmet AYDIN <ahmetaydi@migros.com.tr>
2020-02-09 14:58:57 +0300

    files upload added

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
+import { diskStorage } from 'multer';
+
+const storageOptions = diskStorage({
+  destinaton: './uploads',
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}.${extname(file.originalname)}`);
+  },
+});
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(FilesInterceptor('files', 20, { storage: storageOptions }))
+  async uploadFile(@UploadedFiles() files): Promise<any> {
+    return this.uploadService.upload(files);
+  }
+}
--- tools/environment/environment.ts
+++ tools/environment/environment.ts
@@ -3,4 +3,9 @@ export default {
     'mongodb+srv://udemy:Udemy_123@nestjs-jcbid.mongodb.net/test?retryWrites=true&w=majority',
   hashText: 'myStrongPasswordHashText',
   jwtText: '32dhfnct94q',
+  cloudinary: {
+    cloud_name: 'your_own_cloud_name',
+    api_key: 'your_own_api_key',
+    api_secret: 'your_own_api_secret',
+  },
 };
